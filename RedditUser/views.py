402485bf264641f285c634b9b6330ef8e42d3c5c
mdshepard <mdshepard8@gmail.com>
2019-04-05 14:18:06 -0400

    signup is set up. flash messages done. working on setting up email field on registration form.

--- RedditUser/views.py
+++ RedditUser/views.py
@@ -1,4 +1,7 @@
 from django.contrib.auth import login, authenticate, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from Thread.models import Thread
@@ -8,6 +11,8 @@ from Subreddit.models import Subreddit
 from Vote.models import Vote
 from RedditUser.forms import LoginForm
 from django.views import View
+from django.contrib.auth.models import User
+# from .forms import UserRegisterForm
 
 
 def login_view(request):
@@ -16,6 +21,7 @@ def login_view(request):
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
+            # breakpoint()
             data = form.cleaned_data
             user = authenticate(
                 username=data["username"], password=data["password"]
@@ -25,9 +31,27 @@ def login_view(request):
                 return redirect("/")
     else:
         form = LoginForm()
+    # raise forms.ValidationError("Sorry, that login was invalid. Please try again.")
     return render(request, html, {"form": form})
 
 
 def logout_action(request):
     logout(request)
     return redirect(request.GET.get("next", "/"))
+
+
+def create_user_view(request):
+    form = None
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = User.objects.create_user(username=username, password=password) # noqa
+            RedditUser.objects.create(user=user)
+            messages.success(request, f'Account created for {username}!')
+            return redirect("/")
+    else:
+        form = UserCreationForm()
+    html = "signup.html"
+    return render(request, html, {"form": form})
