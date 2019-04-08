cba514280cd788ab73cd2c12e598172752172eca
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2019-04-08 14:51:56 -0400

    snapshots work with styling

--- redditclone/helpers.py
+++ redditclone/helpers.py
@@ -28,7 +28,10 @@ def get_url_link_thumbnail(post, url):
     driver = webdriver.PhantomJS()
     driver.set_window_size(100, 100)  # set the window size that you need
     driver.get(url)
-    name = url.split("/")[-2]
+    if "www" in url.split(".")[0]:
+        name = url.split(".")[1]
+    else:
+        name = url.split(".")[0]
     img_path = f"{MEDIA_ROOT}/{name}.png"
     driver.save_screenshot(img_path)
     post.link_preview_img = f"{name}.png"
